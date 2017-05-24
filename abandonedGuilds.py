bc9151ed7d46a1780de570a60a2afea7024f4068
mathwhiz <raymond.arndorfer@gmail.com>
2017-05-24 02:51:15 +0000

    Re-implement arguments with argparse

--- abandonedGuilds.py
+++ abandonedGuilds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, getopt, requests, pymustache
+import sys, argparse, requests, pymustache, json
 from datetime import *
 from dateutil.parser import *
 from dateutil.relativedelta import *
@@ -21,30 +21,6 @@ DEFAULT_TEMPLATE = 'wikia.template'
 ABANDONED_LEADER = 6
 
 AUTH = {}
-
-def usage():
-    print('''
-usage: python {} [options]
--h, --help
-    See this message
---uid uid
-    User ID for authentication
---token key
-    API token for authentication. If used, must be with --uid
---personal
-    Flag to indicate searching through user's joined guilds (useful if searching for a guild to help)
--t [file], --template [file]
-    Path to pymustache file for formatting. If not specified, prints in the format of
-    {{Guild Name}}: {{Guild Leader}} - {{Months logged out}}
-    If file is not specified, defaults to {}
--o file, --output file
-    File to output to. Only used with -t or --template
--l num, --limit num
-    Limit the guilds
-    if num is 0: all the guilds
-    if num is positive: limit to the top num guilds (by size)
-    if num is negative: limit to the bottom num guilds (by size)
-'''.format(sys.argv[0], DEFAULT_TEMPLATE))
 
 def get(path, params={}, tried=0):
     r = requests.get(API_BASE + path, params=params, headers=AUTH)
@@ -90,57 +66,22 @@ def formatDate(dateString):
         return 'N/A'
 
 def main():
-    personal = False
-    useTemplate = False
-    limit = 0
-    output = ''
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'htl:o:', ['help', 'uid=', 'token=', 'template', 'personal', 'limit=', 'output='])
-    except getopt.GetoptError as err:
-        print(err)
-        usage()
-        sys.exit(2)
-    for o, a in opts:
-        if o in ('-h', '--help'):
-            usage()
-            sys.exit()
-        elif o == '--uid':
-            AUTH['x-api-user'] = a
-        elif o == '--token':
-            AUTH['x-api-key'] = a
-        elif o in ('-t', '--template'):
-            useTemplate = True
-            templateFile = a if a != '' else DEFAULT_TEMPLATE
-        elif o == '--personal':
-            personal = True
-        elif o in ('-l', '--limit'):
-            limit = int(a)
-        elif o in ('-o', '--output'):
-            output = a
-
-    if (output != '') and not useTemplate:
-        print 'Use -o with --t'
-        usage()
-        sys.exit(2)
+    argparser = argparse.ArgumentParser(description='Search for abandoned Guilds')
+    argparser.add_argument('uid', help='User ID')
+    argparser.add_argument('token', help='API Token')
+    argparser.add_argument('-o', '--output', help='file name to output to')
+    argparser.add_argument('-t', '--template', help='Pymustache template to use as processing', default=DEFAULT_TEMPLATE)
+    argparser.add_argument('--personal', action='store_true', help='search through joined Guilds')
+    args = argparser.parse_args()
     
-    # Authentication
-    if (not ('x-api-user' in AUTH)) and 'x-api-key' in AUTH:
-        print 'Use both --uid and --token, or just --uid'
-        usage()
-        sys.exit(2)
-    elif not ('x-api-user' in AUTH and 'x-api-key' in AUTH):
-        print 'Authentication'
-        if not 'x-api-user' in AUTH:
-            AUTH['x-api-user'] = raw_input('User ID: ')
-        AUTH['x-api-key'] = raw_input('API Token: ')
+    AUTH['x-api-user'] = args.uid
+    AUTH['x-api-key'] = args.token
 
     # Get group IDs
-    allGroups = get('groups', {'type': 'guilds' if personal else 'publicGuilds'})
-
-    groups = (allGroups[:limit] if limit > 0 else allGroups[limit:]) if limit != 0 else allGroups[:]
+    allGroups = get('groups', {'type': 'guilds' if args.personal else 'publicGuilds'})
     
     # Set up progress bar
-    groupsBar = tqdm(groups, unit="guilds")
+    groupsBar = tqdm(allGroups, unit='guilds')
 
     # Loop through each group, and append guilds who have leaders that have been logged out for over ABANDONED_LEADER months
     abandonedGuilds = []
@@ -168,7 +109,7 @@ def main():
     # Sort by id
     abandonedGuilds_sorted = sorted(abandonedGuilds, key=lambda guild: guild['guild']['_id'])
     
-    if not useTemplate:
+    if not args.template:
         for abandonedGuild in abandonedGuilds_sorted:
             print u'{}: {} - {}'.format(abandonedGuild['guild']['name'], abandonedGuild['leader']['profile']['name'], abandonedGuild['abandoned'].years*12 + abandonedGuild['abandoned'].months)
     else:
@@ -195,13 +136,13 @@ def main():
             'guilds': guilds,
             'today': date.today().isoformat()
         }
-        with open(templateFile, 'r') as t:
+        with open(args.template, 'r') as t:
             compiledTemplate = pymustache.compiled(t.read())
             compiledTemplate.filters['date'] = lambda date: formatDate(date)
             outputData = compiledTemplate.render(data)
         
-        if output != '':
-            with open(output, 'w+') as outputFile:
+        if args.output != '':
+            with open(args.output, 'w+') as outputFile:
                 outputFile.write(outputData)
         else:
             print outputData
